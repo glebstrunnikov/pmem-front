50a761fff7c5f6b466d58344dee160c9bf99d895
glebstrunnikov <glebstrunnikov@gmail.com>
2025-10-29 17:05:58 +0200

    bugfix img

--- app/components/ContentArea.vue
+++ app/components/ContentArea.vue
@@ -1,103 +1,101 @@
 <script setup lang="ts">
-  const config = useRuntimeConfig();
-  const page = ref(1);
-  const firstPosts = ref<Post[]>([]);
-  const additionalPosts = ref<Post[]>([]);
-  const showPagnator = useState("showPagnator", () => true);
-  const locale = useState("locale");
-  const route = useRoute();
-  const i18n = useI18n();
-  const props = withDefaults(
-    defineProps<{
-      contentClass?: string;
-    }>(),
-    {
-      contentClass:
-        "grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-14 pt-16 bg-background mb-25",
-    }
-  );
-  const query = computed(() => {
-    return route.query.query as string;
-  });
+const config = useRuntimeConfig();
+const page = ref(1);
+const firstPosts = ref<Post[]>([]);
+const additionalPosts = ref<Post[]>([]);
+const showPagnator = useState("showPagnator", () => true);
+const locale = useState("locale");
+const route = useRoute();
+const i18n = useI18n();
+const props = withDefaults(
+  defineProps<{
+    contentClass?: string;
+  }>(),
+  {
+    contentClass:
+      "grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-14 pt-16 bg-background mb-25",
+  }
+);
+const query = computed(() => {
+  return route.query.query as string;
+});
 
-  const sort = computed(() => {
-    const currentSort = route.query.sort;
-    if (currentSort === "new" || !currentSort) {
-      return "publishedAt:desc";
+const sort = computed(() => {
+  const currentSort = route.query.sort;
+  if (currentSort === "new" || !currentSort) {
+    return "publishedAt:desc";
+  } else {
+    return "publishedAt:asc";
+  }
+});
+
+const tagSlug = computed(() => {
+  if (route.fullPath.startsWith("/tags/") && route.params.slug) {
+    return route.params.slug as string;
+  }
+});
+
+const { data, error, pending } = await useFetch<StrapiResponse>(
+  () =>
+    `/posts?locale=${locale.value}${query.value
+      ? `&filters[$or][0][title][$containsi]=${query.value}&filters[$or][1][content][$containsi]=${query.value}`
+      : ""
+    }${tagSlug.value ? `&filters[tags][slug][$eq]=${tagSlug.value}` : ""}${sort.value ? `&sort=${sort.value}` : ""
+    }&pagination[page]=${page.value}&pagination[pageSize]=4&populate=*`,
+  {
+    baseURL: config.public.apiBase,
+    watch: [page, locale, query, sort],
+  }
+);
+const allPosts = computed(() => {
+  if (page.value === 1) {
+    return data.value?.data || [];
+  } else {
+    return [...firstPosts.value, ...additionalPosts.value];
+  }
+});
+watch(locale, () => {
+  page.value = 1;
+});
+watch(query, () => {
+  page.value = 1;
+});
+watch(sort, () => {
+  page.value = 1;
+});
+watch(
+  data,
+  (newData) => {
+    if (!!newData?.data && page.value === 1) {
+      firstPosts.value = newData.data;
+    }
+    if (newData?.data && page.value > 1) {
+      additionalPosts.value = [...additionalPosts.value, ...newData.data];
+    }
+    if (!!newData?.data && newData?.data.length < 4) {
+      showPagnator.value = false;
     } else {
-      return "publishedAt:asc";
+      showPagnator.value = true;
     }
-  });
-
-  const tagSlug = computed(() => {
-    if (route.fullPath.startsWith("/tags/") && route.params.slug) {
-      return route.params.slug as string;
-    }
-  });
-
-  const { data, error, pending } = await useFetch<StrapiResponse>(
-    () =>
-      `/posts?locale=${locale.value}${
-        query.value
-          ? `&filters[$or][0][title][$containsi]=${query.value}&filters[$or][1][content][$containsi]=${query.value}`
-          : ""
-      }${tagSlug.value ? `&filters[tags][slug][$eq]=${tagSlug.value}` : ""}${
-        sort.value ? `&sort=${sort.value}` : ""
-      }&pagination[page]=${page.value}&pagination[pageSize]=4&populate=*`,
+  },
+  { immediate: true }
+);
+useHead({
+  meta: [
     {
-      baseURL: config.public.apiBase,
-      watch: [page, locale, query, sort],
-    }
-  );
-  const allPosts = computed(() => {
-    if (page.value === 1) {
-      return data.value?.data || [];
-    } else {
-      return [...firstPosts.value, ...additionalPosts.value];
-    }
-  });
-  watch(locale, () => {
-    page.value = 1;
-  });
-  watch(query, () => {
-    page.value = 1;
-  });
-  watch(sort, () => {
-    page.value = 1;
-  });
-  watch(
-    data,
-    (newData) => {
-      if (!!newData?.data && page.value === 1) {
-        firstPosts.value = newData.data;
-      }
-      if (newData?.data && page.value > 1) {
-        additionalPosts.value = [...additionalPosts.value, ...newData.data];
-      }
-      if (!!newData?.data && newData?.data.length < 4) {
-        showPagnator.value = false;
-      } else {
-        showPagnator.value = true;
-      }
+      property: "og:image",
+      content: `${config.public.url}/smlogo.png`,
     },
-    { immediate: true }
-  );
-  useHead({
-    meta: [
-      {
-        property: "og:image",
-        content: `${config.public.url}/smlogo.png`,
-      },
-      {
-        name: "twitter:image",
-        content: `${config.public.url}/smlogo.png`,
-      },
-    ],
-  });
+    {
+      name: "twitter:image",
+      content: `${config.public.url}/smlogo.png`,
+    },
+  ],
+});
 
-  const loadMore = () => {
-    page.value++;
-  };
+const loadMore = () => {
+  page.value++;
+};
 </script>
 <template>
   <div class="w-full flex flex-col">
@@ -110,10 +108,7 @@
     <!-- <br /> -->
     <!-- {{ tagSlug }} -->
 
-    <div
-      v-if="(query || tagSlug) && !data?.data.length"
-      class="w-full flex flex-col items-center text-center mt-18"
-    >
+    <div v-if="(query || tagSlug) && !data?.data.length" class="w-full flex flex-col items-center text-center mt-18">
       <div class="text-title-xxl text-tomato mb-11">
         {{ i18n.t("nothingFound") }}
       </div>
@@ -123,16 +118,9 @@
       <div class="text-title-xxl text-primary">pmemeuropa@gmail.com</div>
     </div>
     <div v-if="data" :class="contentClass">
-      <CardArticleCard
-        v-for="post in allPosts"
-        :key="post.id"
-        :title="post.title"
-        :description="post.lead"
-        :imageUrl="config.public.url + post.cover?.formats?.medium.url"
-        :date="post.publishedAt"
-        :link="'/posts/' + post.slug"
-        :tags="post.tags"
-      />
+      <CardArticleCard v-for="post in allPosts" :key="post.id" :title="post.title" :description="post.lead"
+        :imageUrl="config.public.url + post.cover?.url" :date="post.publishedAt" :link="'/posts/' + post.slug"
+        :tags="post.tags" />
     </div>
     <div v-if="data && showPagnator" class="w-full flex justify-center mb-20">
       <Paginator @click="loadMore" :isLoading="pending" />
